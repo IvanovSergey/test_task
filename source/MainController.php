f36d602d4c031ef1d0886fa4fd5e60e4d4f60650
Ivanov Sergey <mariupolecbivshiy@gmail.com>
2018-08-31 11:51:01 +0300

    Mirafox test_task

--- source/MainController.php
+++ source/MainController.php
@@ -1,61 +1,443 @@
 <?php 
-include 'source/DriversController.php';
-include 'source/CarsController.php';
 
 class Main { 
-    public $places,
-    $drivers, 
-    $cars,
-    $ranges = array("30", "90", "360"),
-    $output;
+    public $output,
+    $teams =array(
+	0 => array(
+		'name' => 'Бразилия', # название команды
+		'games' => 104, # всего игр
+		'win' => 70, # количество побед
+		'draw' => 17, # количестве ничей
+		'defeat' => 17, # количество поражений
+		'goals' => array(
+			'scored' => 221, # всего забито голов
+			'skiped' => 102, # всего пропущено голов
+		),
+	),
+	1 => array(
+		'name' => 'Германия / ФРГ',
+		'games' => 106,
+		'win' => 66,
+		'draw' => 20,
+		'defeat' => 20,
+		'goals' => array(
+			'scored' => 224,
+			'skiped' => 121,
+		),
+	),
+	2 => array(
+		'name' => 'Италия',
+		'games' => 83,
+		'win' => 45,
+		'draw' => 21,
+		'defeat' => 17,
+		'goals' => array(
+			'scored' => 128,
+			'skiped' => 77,
+		),
+	),
+	3 => array(
+		'name' => 'Аргентина',
+		'games' => 77,
+		'win' => 42,
+		'draw' => 14,
+		'defeat' => 21,
+		'goals' => array(
+			'scored' => 131,
+			'skiped' => 84,
+		),
+	),
+	4 => array(
+		'name' => 'Англия',
+		'games' => 62,
+		'win' => 26,
+		'draw' => 20,
+		'defeat' => 16,
+		'goals' => array(
+			'scored' => 79,
+			'skiped' => 56,
+		),
+	),
+	5 => array(
+		'name' => 'Испания',
+		'games' => 59,
+		'win' => 29,
+		'draw' => 12,
+		'defeat' => 18,
+		'goals' => array(
+			'scored' => 92,
+			'skiped' => 66,
+		),
+	),
+	6 => array(
+		'name' => 'Франция',
+		'games' => 59,
+		'win' => 28,
+		'draw' => 12,
+		'defeat' => 19,
+		'goals' => array(
+			'scored' => 106,
+			'skiped' => 71,
+		),
+	),
+	7 => array(
+		'name' => 'Голландия',
+		'games' => 50,
+		'win' => 27,
+		'draw' => 12,
+		'defeat' => 11,
+		'goals' => array(
+			'scored' => 86,
+			'skiped' => 48,
+		),
+	),
+	8 => array(
+		'name' => 'Уругвай',
+		'games' => 51,
+		'win' => 20,
+		'draw' => 12,
+		'defeat' => 19,
+		'goals' => array(
+			'scored' => 80,
+			'skiped' => 71,
+		),
+	),
+	9 => array(
+		'name' => 'Швеция',
+		'games' => 46,
+		'win' => 16,
+		'draw' => 13,
+		'defeat' => 17,
+		'goals' => array(
+			'scored' => 74,
+			'skiped' => 69,
+		),
+	),
+	10 => array(
+		'name' => 'Россия / СССР',
+		'games' => 40,
+		'win' => 17,
+		'draw' => 8,
+		'defeat' => 15,
+		'goals' => array(
+			'scored' => 66,
+			'skiped' => 47,
+		),
+	),
+	11 => array(
+		'name' => 'Сербия / Югославия    ',
+		'games' => 43,
+		'win' => 17,
+		'draw' => 8,
+		'defeat' => 18,
+		'goals' => array(
+			'scored' => 64,
+			'skiped' => 59,
+		),
+	),
+	12 => array(
+		'name' => 'Мексика',
+		'games' => 53,
+		'win' => 14,
+		'draw' => 14,
+		'defeat' => 25,
+		'goals' => array(
+			'scored' => 57,
+			'skiped' => 92,
+		),
+	),
+	13 => array(
+		'name' => 'Бельгия',
+		'games' => 41,
+		'win' => 14,
+		'draw' => 9,
+		'defeat' => 18,
+		'goals' => array(
+			'scored' => 52,
+			'skiped' => 66,
+		),
+	),
+	14 => array(
+		'name' => 'Польша',
+		'games' => 31,
+		'win' => 15,
+		'draw' => 5,
+		'defeat' => 11,
+		'goals' => array(
+			'scored' => 44,
+			'skiped' => 40,
+		),
+	),
+	15 => array(
+		'name' => 'Венгрия',
+		'games' => 32,
+		'win' => 15,
+		'draw' => 3,
+		'defeat' => 14,
+		'goals' => array(
+			'scored' => 87,
+			'skiped' => 57,
+		),
+	),
+	16 => array(
+		'name' => 'Португалия',
+		'games' => 26,
+		'win' => 13,
+		'draw' => 4,
+		'defeat' => 9,
+		'goals' => array(
+			'scored' => 43,
+			'skiped' => 29,
+		),
+	),
+	17 => array(
+		'name' => 'Чехия / Чехословакия',
+		'games' => 33,
+		'win' => 12,
+		'draw' => 5,
+		'defeat' => 16,
+		'goals' => array(
+			'scored' => 47,
+			'skiped' => 49,
+		),
+	),
+	18 => array(
+		'name' => 'Чили',
+		'games' => 33,
+		'win' => 11,
+		'draw' => 7,
+		'defeat' => 15,
+		'goals' => array(
+			'scored' => 40,
+			'skiped' => 49,
+		),
+	),
+	19 => array(
+		'name' => 'Австрия',
+		'games' => 29,
+		'win' => 12,
+		'draw' => 4,
+		'defeat' => 13,
+		'goals' => array(
+			'scored' => 43,
+			'skiped' => 47,
+		),
+	),
+	20 => array(
+		'name' => 'Швейцария',
+		'games' => 33,
+		'win' => 11,
+		'draw' => 6,
+		'defeat' => 16,
+		'goals' => array(
+			'scored' => 45,
+			'skiped' => 59,
+		),
+	),
+	21 => array(
+		'name' => 'Парагвай',
+		'games' => 27,
+		'win' => 7,
+		'draw' => 10,
+		'defeat' => 10,
+		'goals' => array(
+			'scored' => 30,
+			'skiped' => 38,
+		),
+	),
+	22 => array(
+		'name' => 'США',
+		'games' => 33,
+		'win' => 8,
+		'draw' => 6,
+		'defeat' => 19,
+		'goals' => array(
+			'scored' => 37,
+			'skiped' => 62,
+		),
+	),
+	23 => array(
+		'name' => 'Румыния',
+		'games' => 21,
+		'win' => 8,
+		'draw' => 5,
+		'defeat' => 8,
+		'goals' => array(
+			'scored' => 30,
+			'skiped' => 32,
+		),
+	),
+	24 => array(
+		'name' => 'Южная Корея',
+		'games' => 31,
+		'win' => 5,
+		'draw' => 9,
+		'defeat' => 17,
+		'goals' => array(
+			'scored' => 31,
+			'skiped' => 67,
+		),
+	),
+	25 => array(
+		'name' => 'Дания',
+		'games' => 16,
+		'win' => 8,
+		'draw' => 2,
+		'defeat' => 6,
+		'goals' => array(
+			'scored' => 27,
+			'skiped' => 24,
+		),
+	),
+	26 => array(
+		'name' => 'Хорватия',
+		'games' => 16,
+		'win' => 7,
+		'draw' => 2,
+		'defeat' => 7,
+		'goals' => array(
+			'scored' => 21,
+			'skiped' => 17,
+		),
+	),
+	27 => array(
+		'name' => 'Колумбия',
+		'games' => 18,
+		'win' => 7,
+		'draw' => 2,
+		'defeat' => 9,
+		'goals' => array(
+			'scored' => 26,
+			'skiped' => 27,
+		),
+	),
+	28 => array(
+		'name' => 'Шотландия',
+		'games' => 23,
+		'win' => 4,
+		'draw' => 7,
+		'defeat' => 12,
+		'goals' => array(
+			'scored' => 25,
+			'skiped' => 41,
+		),
+	),
+	29 => array(
+		'name' => 'Камерун',
+		'games' => 23,
+		'win' => 4,
+		'draw' => 7,
+		'defeat' => 12,
+		'goals' => array(
+			'scored' => 18,
+			'skiped' => 43,
+		),
+	),
+	30 => array(
+		'name' => 'Коста-Рика',
+		'games' => 15,
+		'win' => 5,
+		'draw' => 4,
+		'defeat' => 6,
+		'goals' => array(
+			'scored' => 17,
+			'skiped' => 23,
+		),
+	),
+	31 => array(
+		'name' => 'Болгария',
+		'games' => 26,
+		'win' => 3,
+		'draw' => 8,
+		'defeat' => 15,
+		'goals' => array(
+			'scored' => 22,
+			'skiped' => 53,
+		),
+	),	      
+    ),
+        $average_scored,
+        $average_skipped;
     
-    function __construct($input) { 
-        $input = json_decode($input, true);
-        if(!isset($input['park']) || !isset($input['drivers']) || !isset($input['cars']))
-            return false;
-        $this->places = $input['park']['places'];
-        $this->drivers = new Drivers($input['drivers']);
-        $this->cars = new Cars($input["cars"]); 
-        if(isset($input['params']) && isset($input['params']['range'])){
-            array_push($this->ranges, $input['params']['range']);
-        } 
-        $this->ready_cars = array_map(function ($a1, $b1) { return $a1 + $b1; }, $this->drivers->drivers, $this->cars->cars); // усаживаем водителей за руль                
-    } 
-    
-    function calculate(){
-        foreach($this->ready_cars as $key=>$car){
-            $this->ready_cars[$key] = $this->drivers->calculate($this->ready_cars[$key]);            
-            $this->ready_cars[$key] = $this->cars->calculate($this->ready_cars[$key]);            
-        }
-        $this->output['places'] = $this->places;
-        $this->output['total_cars'] = count($this->ready_cars);
-        $this->output['bivaliy'] = $this->drivers->experiensed;
-        $this->output['casual'] = $this->drivers->casual;
-        $this->output['luda'] = $this->cars->luda;
-        $this->output['homba'] = $this->cars->homba;
-        $this->output['hendai'] = $this->cars->hendai;
-        $this->output['ranges'] = $this->ranges;
-        $this->output['sum_distance'] = array_sum(array_column($this->ready_cars, 'distance'));
-        $this->output['sum_initial_distance'] = array_sum(array_column($this->ready_cars, 'km'));
-        $this->output['sum_durability'] = array_sum(array_column($this->ready_cars, 'durability'));
-        $this->output['sum_initial_durability'] = array_sum(array_column($this->ready_cars, 'initial_durability'));
-        $this->output['suggested_places'] = ceil(($this->output['sum_durability'] + $this->output['sum_initial_durability'])/100);
-        $this->output['suggested_places_without_initial'] = ceil(($this->output['sum_durability'])/100);
-        $this->output['sum_gas'] = array_sum(array_column($this->ready_cars, 'gas'));
-        
-        foreach($this->ranges as $range){
-            $this->calculateByRange($range);
-        }
-        return $this->output;
+    function __construct() { 
+        $this->init_average();                
     }
     
-    function calculateByRange($range){ 
-        $this->output[$range . 'days']['sum_distance'] = round($this->output['sum_distance']*$range + $this->output['sum_initial_distance'],2);
-        $this->output[$range . 'days']['sum_durability'] = round(($this->output['sum_durability']*$range + $this->output['sum_initial_durability'] + $this->cars->initial_break_value*$range)/100,2); // рассчитываем вероятность поломки с учетом выбранного промежутка времени, начальной прогнозируемойy поломки от уже пройденного расстояния и стандартного начального коэф. в 0.5% в день
-        $this->output[$range . 'days']['sum_initial_durability'] = round($this->output['sum_initial_durability']/100,2);
-        $this->output[$range . 'days']['all_repair_days'] = round($this->output[$range . 'days']['sum_durability']*3,2);
-        $this->output[$range . 'days']['repair_days'] = round(($this->output[$range . 'days']['sum_durability'] - $this->output['sum_initial_durability']/100)*3,2);
-        $this->output[$range . 'days']['sum_gas'] = round($this->output['sum_gas']*$range,2);        
+    function calculate($c1, $c2){
+        $team1 = $this->powerCalculate($c1);
+        $team2 = $this->powerCalculate($c2);
+        
+        $team1['avg_goal_prob'] = $team1['atack_power']*$team2['defense_power']*$this->average_scored;
+        $team2['avg_goal_prob'] = $team2['atack_power']*$team1['defense_power']*$this->average_scored;
+        
+        $probs = $this->prob_calculator($team1['avg_goal_prob'], $team2['avg_goal_prob']);
+        
+        return $this->result_calculator($probs);
+    }
+    
+    function result_calculator($probs){
+        function randomFloat($min = 0, $max = 1) {
+            return $min + mt_rand() / mt_getrandmax() * ($max - $min);
+        }
+        
+        foreach($probs as $key=>$goal_probs){
+            $sumOfWeights = array_sum($goal_probs);
+            $random = randomFloat(0, $sumOfWeights);  
+            do {
+                $sumOfWeights = array_sum($goal_probs);
+                if($random <= $sumOfWeights && $random >= $sumOfWeights - end($goal_probs)) {
+                    $result[$key] = key($goal_probs);
+                }
+            } while(array_pop($goal_probs));
+        }
+                
+        return $result;
+    }
+    
+    function prob_calculator($prob1, $prob2){
+        $team1[] = exp(-$prob1); 
+        $team1[] = $prob1*exp(-$prob1);
+        $team2[] = exp(-$prob2); 
+        $team2[] = $prob2*exp(-$prob2);
+        
+        for ($i = 2; $i <= 5; $i++) {
+            $team1[$i] = 1/$this->factorial($i)* pow($prob1, $i)* exp(-$prob1);
+            $team2[$i] = 1/$this->factorial($i)* pow($prob2, $i)* exp(-$prob2);
+        }
+        
+        return array($team1, $team2);
+    }
+    
+    function factorial($x){
+        $result = 1 ;
+        while ( $x >= 2 ) $result *= $x-- ;
+        
+        return $result ;
+    }
+    
+    function init_average(){
+        $games = array_sum(array_column($this->teams, 'games'));
+        
+        $scored = array_reduce($this->teams, function($carry, $item) {
+            $carry += $item['goals']['scored'];
+            return $carry;
+        });
+        
+        $skiped = array_reduce($this->teams, function($carry, $item) {
+            $carry += $item['goals']['skiped'];
+            return $carry;
+        });
+        
+        $this->average_scored = $scored/$games;        
+        $this->average_skiped = $skiped/$games;
+    }
+    
+    function powerCalculate($id){                    
+        $team = $this->teams[$id];
+        $team['atack_power'] = ($team['goals']['scored']/$team['games'])/$this->average_scored;
+        $team['defense_power'] = ($team['goals']['skiped']/$team['games'])/$this->average_skiped;
+        
+        return $team;        
     }
 } 
  
